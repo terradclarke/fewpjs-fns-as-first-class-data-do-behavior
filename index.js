e7d5d373a0fb21fa855414375f132c4d939d2242
Terradclarke <terranie67@gmail.com>
2020-11-17 16:11:45 -0600

    commit tree e598b19cf23de4b7e4c9a248186d70af4cd4e126

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(str) {
+  let hour = str.split(':')[0];
+  if (hour < 0) throw new RangeError("Sorry, that's not a valid time!");
+  if (hour < 12) return "Good Morning";
+  if (hour > 17) return "Good Evening";
+  return "Good Afternoon";
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  document.getElementById('greeting').innerText = str;
+}
